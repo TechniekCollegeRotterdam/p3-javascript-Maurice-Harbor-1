d92cbff98ba0fdeb6ae8bc7a6c47c84b6627161b
Maurice Harbor <9017060@student.zadkine.nl>
2023-02-16 10:22:27 +0100

    opdracht2.4

---
+++ opdracht2.4/app.js
@@ -0,0 +1,44 @@
+// let x = 10;
+// let y = 5;
+
+// let z = function(a,b)
+// {
+//    return x + y
+// };
+
+// console.log("the awnser is " + z)
+
+// let x = 10;
+// let y = 5;
+// let z = x*y;
+
+// let z = function(){
+//     let z = x * y; 
+// };
+
+// console.log(z);
+
+function calculate(){
+    let x = 10;
+    let y = 5;
+    let z = x + y;
+};
+
+console.log(calculate);
+
+
+function multiplyByFive(){
+    let x = 10;
+    let y = 5;
+    let z = x * y;
+};
+
+console.log(multiplyByFive);
+
+function minute(){
+    let x = 120;
+    let y = 60;
+    let z = x / y;
+};
+
+console.log(minute);
