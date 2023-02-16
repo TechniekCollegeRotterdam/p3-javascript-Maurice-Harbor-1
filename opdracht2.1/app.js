b1937185ed9b51614ed93041651501a4c00dc36b
Maurice Harbor <9017060@student.zadkine.nl>
2023-02-16 09:09:31 +0100

    1st push test

---
+++ opdracht2.1/app.js
@@ -0,0 +1,8 @@
+let title = document.getElementById("title");
+let button = document.getElementById("button");
+
+let kleur_aanpassen = function(){
+
+}
+
+kleur_aanpassen();
---
+++ week 2-uitleg/app.js
@@ -0,0 +1,10 @@
+let title = document.getElementById("title");
+let image = document.getElementById("image");
+let button = document.getElementById("button");
+
+title.style.color = "gold";
+document.getElementById("image").style.display = "";
+
+document.getElementById("button");
+
+
