9183e600b19726f40d36f854861db9e63287d195
Maurice Harbor <9017060@student.zadkine.nl>
2023-02-16 09:38:43 +0100

    opdracht2.3

---
+++ opdracht2.3/app.js
@@ -0,0 +1,7 @@
+let title = document.getElementById("title");
+
+document.getElementById("title").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("title").style.color = "gold";
+}
